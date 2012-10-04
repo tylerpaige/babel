04c0d65de4bc00e4a8c3fbfef85335e85bd0bb16
Tyler Paige <tylerclarkpaige@gmail.com>
2012-10-04 18:09:29 -0400

    Layout established
    Needs to talk to twitter still

---
+++ files/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>This is It</title>
+		<link rel="stylesheet/less" type="text/css" href="css/babel.less">
+		<script src="http://code.jquery.com/jquery-1.8.1.min.js"></script>
+		<script src="js/less-1.3.0.min.js" type="text/javascript" />
+		
+	</head>
+	<body>
+		<navigation>
+			<li><a href="#" class="active">Contribute</a></li>
+			<li><a href="#">Search</a></li>
+			<li><a href="#">View Current Dictionary</a></li>
+		</navigation>
+		<div id="container">
+			<header>
+				<a href="index.php"><img src="images/logo.png" /></a>
+			</header>
+			<article id="content">
+				<section id="instruction">
+					Help define this symbol:
+					<h1 class="symbol">(∏ ω ∏)</h1>
+				</section>
+				<ol>
+					<li class="definition">
+						<img src="#" class="defImg" />
+					</li>
+				</ol>
+			</article>
+		</div>
+	</body>
+</html>
